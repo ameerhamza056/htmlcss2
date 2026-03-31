0ad2729481bff8ceaf560117076b63fe68f9e6fb
ameerhamza056 <hamzaawaismughal@gmail.com>
2026-03-31 19:52:34 +0500

    Add files via upload

---
+++ src/Components/Html.jsx
@@ -0,0 +1,60 @@
+export default function Html(){
+    return(
+        <>
+         <h2 align="center">My Resume </h2>
+
+     <table border="1" align="center" cellpadding="10">
+        <tr>
+            <td rowspan="3" width="150" align="center">
+            <img src="c:\Users\abdul\Pictures\Camera Roll\profilepic.JPG" alt="my photo" width="200" height="200"/>
+            </td>
+            <td><b>Name:</b> Ameer Hamza </td>
+        </tr>
+        <tr>
+            <td><b>Email:</b> hamzaawaismughal@gmail.com</td>
+        </tr>
+        <tr>
+            <td><b>Phone:</b> +923337877845</td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <b>Career Objective</b>
+                To obtain <strong>a challenging</strong> position where I can use my skills and grow professionally.
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2"><b>Education</b></td>
+        </tr>
+        <tr>
+            <td>2025 - 2029</td>
+            <td>Bachelor of <em>Science</em>  in Computer Science</td>
+        </tr>
+        <tr>
+            <td>2021 - 2025</td>
+            <td>Matric + Inter</td>
+        </tr>
+        <tr>
+            <td colspan="2"><b>Skills</b></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                HTML, CSS, JavaScript, C++, Python
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2"><b>Experience</b></td>
+        </tr>
+        <tr>
+            <td>2026</td>
+            <td>Interned at corvit </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <b>Declaration:</b> 
+                I hereby declare that the above information is true to the best of my knowledge.            
+            </td>
+        </tr>
+    </table>
+        </>
+    )
+}
